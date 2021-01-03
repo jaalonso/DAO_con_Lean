b3684d5e30d8f95ccfc39b58501e03ff99bc61ac
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-01-03 18:25:16 +0100

    Límites

--- src/5_Limites/Igualdad_a_partir_de_acotaciones_del_valor_absoluto_de_la_diferencia.lean
+++ src/5_Limites/Igualdad_a_partir_de_acotaciones_del_valor_absoluto_de_la_diferencia.lean
@@ -12,14 +12,38 @@ variables (x y : ℝ)
 
 notation `|`x`|` := abs x
 
+-- ?ª demostración
+example :
+  (∀ ε > 0, |x - y| ≤ ε) → x = y :=
+begin
+  intro h,
+  apply eq_of_abs_sub_nonpos,
+  by_contradiction h1,
+  push_neg at h1,
+  have h2 : |x - y|/2 > 0,
+    { exact half_pos h1, },
+  specialize h (|x - y|/2) h2,
+  have h3 : |x - y| > |x - y|/2 ,
+    { exact half_lt_self h1, },
+  contrapose h3,
+  exact not_lt_of_le h,
+end
 
-lemma ig_de_abs_sub_me_todos : -- eq_of_abs_sub_le_all :
+-- ?ª demostración
+example :
+  (∀ ε > 0, |x - y| ≤ ε) → x = y :=
+assume h1 : ∀ ε > 0, |x - y| ≤ ε,
+suffices |x - y| ≤ 0,
+  from eq_of_abs_sub_nonpos,
+sorry
+
+lemma ig_de_abs_sub_mne_todos :
   (∀ ε > 0, |x - y| ≤ ε) → x = y :=
 begin
   intro h,
   apply eq_of_abs_sub_nonpos,
   by_contradiction H,
   push_neg at H,
-  specialize h ( |x-y|/2) (by linarith),
+  specialize h (|x-y|/2) (by linarith),
   linarith,
 end
---
+++ src/5_Limites/cero_de_abs_mn_todos.lean
@@ -0,0 +1,146 @@
+-- Si |x| < ε, para todo ε > 0, entonces x = 0
+-- ===========================================
+
+import data.real.basic
+
+variable (x : ℝ)
+
+-- ----------------------------------------------------
+-- Ejercicio 1. Definir la notación |x| para el valor
+-- absoluto de x.
+-- ----------------------------------------------------
+
+notation `|`x`|` := abs x
+
+-- ----------------------------------------------------
+-- Ejercicio 2. Demostrar que si |x| < ε, para todo
+-- ε > 0, entonces x = 0
+-- ----------------------------------------------------
+
+-- 1ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+begin
+  rw ← abs_eq_zero,
+  apply eq_of_le_of_forall_le_of_dense,
+  { exact abs_nonneg x, },
+  { intros ε hε,
+    apply le_of_lt,
+    exact h ε hε, },
+end
+
+-- 2ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+begin
+  rw ← abs_eq_zero,
+  apply eq_of_le_of_forall_le_of_dense,
+  { exact abs_nonneg x, },
+  { intros ε hε,
+    exact le_of_lt (h ε hε), },
+end
+
+-- 3ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+begin
+  rw ← abs_eq_zero,
+  apply eq_of_le_of_forall_le_of_dense,
+  { exact abs_nonneg x, },
+  { exact λ ε hε, le_of_lt (h ε hε), },
+end
+
+-- 4ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+begin
+  rw ← abs_eq_zero,
+  apply eq_of_le_of_forall_le_of_dense
+        (abs_nonneg x)
+        (λ ε hε, le_of_lt (h ε hε)),
+end
+
+-- 5ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+abs_eq_zero.mp
+  (eq_of_le_of_forall_le_of_dense
+    (abs_nonneg x)
+    (λ ε hε, le_of_lt (h ε hε)))
+
+-- 6ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+have h1 : 0 ≤ |x|,
+  from abs_nonneg x,
+have h2 : ∀ ε, ε > 0 → |x| ≤ ε,
+  { assume ε,
+    assume hε : ε > 0,
+    have h2a : |x| < ε,
+      from h ε hε,
+    show |x| ≤ ε,
+      from le_of_lt h2a },
+have h3 : |x| = 0,
+  from eq_of_le_of_forall_le_of_dense h1 h2,
+show x = 0,
+  from abs_eq_zero.mp h3
+
+-- 7ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+have h1 : 0 ≤ |x|,
+  from abs_nonneg x,
+have h2 : ∀ ε, ε > 0 → |x| ≤ ε,
+  { assume ε,
+    assume hε : ε > 0,
+    have h2a : |x| < ε,
+      from h ε hε,
+    show |x| ≤ ε,
+      from le_of_lt h2a },
+have h3 : |x| = 0,
+  from eq_of_le_of_forall_le_of_dense h1 h2,
+abs_eq_zero.mp h3
+
+-- 8ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+have h1 : 0 ≤ |x|,
+  from abs_nonneg x,
+have h2 : ∀ ε, ε > 0 → |x| ≤ ε,
+  { assume ε,
+    assume hε : ε > 0,
+    have h2a : |x| < ε,
+      from h ε hε,
+    show |x| ≤ ε,
+      from le_of_lt h2a },
+abs_eq_zero.mp (eq_of_le_of_forall_le_of_dense h1 h2)
+
+-- 9ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+have h1 : 0 ≤ |x|,
+  from abs_nonneg x,
+have h2 : ∀ ε, ε > 0 → |x| ≤ ε,
+  { assume ε,
+    assume hε : ε > 0,
+    show |x| ≤ ε,
+      from le_of_lt (h ε hε) },
+abs_eq_zero.mp (eq_of_le_of_forall_le_of_dense h1 h2)
+
+-- 10ª demostración
+example
+  (h : ∀ ε > 0, |x| < ε)
+  : x = 0 :=
+abs_eq_zero.mp
+  (eq_of_le_of_forall_le_of_dense
+    (abs_nonneg x)
+    (λ ε hε, le_of_lt (h ε hε)))
